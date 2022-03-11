936a7538b992401373843d34997007ad7a88f9c3
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2022-03-11 14:59:00 +0100

    Parprogrammera : ingredient model

---
+++ database/migrations/2022_03_11_134701_create_ingredients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('ingredient');
+            $table->foreignId('recipe_id')->references('id')->on('recipes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
