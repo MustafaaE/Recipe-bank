69402447ef2dbdc11ca7038f6c5ce35acf157f03
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2022-03-12 17:56:44 +0100

    ingredient-recipe-model created -ParP

---
+++ database/migrations/2022_03_12_164235_create_recipe_ingredient_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipeIngredientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe_ingredient', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('recipe_id')->references('id')->on('recipes');
+            $table->foreignId('ingredient_id')->references('id')->on('ingredients');
+            $table->integer('amount');
+            $table->string('unit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe_ingredient');
+    }
+}
