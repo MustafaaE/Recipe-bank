99c8d003b16d72408e7fc9fd4557656743a0c224
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2022-03-14 16:41:51 +0100

    change signup route ParP

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -11,10 +11,10 @@ use App\Models\Ingredient;
 class RecipeController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Display a listing of the resource.
