7a509b4657c61317abafa22cbca2553cb2229749
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2022-03-27 10:15:22 +0200

    added README.md

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -10,9 +10,11 @@
                 <div id="postcard-bg" class="card w-50 mb-4">
                         <h2 id="recipetitle" class="card-header d-flex flex-row justify-content-center"> <a class="card-title text-decoration-none" id="h2title" href="/recipes/{{ $recipe->id }}">{{ $recipe->title }}</h2></a>
                         <a class="text-decoration-none" href="/recipes/{{ $recipe->id }}">
-                            @if($recipe->image)
-                    <img class="card-img-top text-decoration-none" src="{{ URL('storage/images/'. $recipe->image)}}" alt="test" width="500px" height="550px"> </a>
-                    @endif
+                            @if ($recipe->image)
+                                <img class="card-img-top text-decoration-none"
+                                    src="{{ URL('storage/images/' . $recipe->image) }}" alt="test" width="500px" height="500px">
+                            @endif
+                        </a>
                     <h3 class="d-flex flex-row justify-content-center">{{ $recipe->description }}</h3>
                     <div class="d-flex flex-row justify-content-between">
                         <span>Uploaded by: {{ $recipe->user->name }}</span>
